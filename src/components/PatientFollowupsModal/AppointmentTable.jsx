de2b219106fd8b65a57fb5cce86b9f697454baad
= <=>
2023-06-05 11:58:41 +0100

    separte AppointmentTable and add some state

---
+++ src/components/PatientFollowupsModal/AppointmentTable.jsx
@@ -0,0 +1,103 @@
+import { useState } from 'react'
+import { Button } from '@chakra-ui/react'
+import { format, parseISO } from 'date-fns'
+
+import { ADD_APPOINTMENT_NAMES } from '../../config'
+
+export default function AppointmentTable({ appointmentsGroup }) {
+  const [baseAppointment] = appointmentsGroup
+  const totalPayments = appointmentsGroup.reduce((total, appointment) => total + appointment.payment, 0)
+  const [canShowSaveBtn, setCanShowSaveBtn] = useState(false)
+
+  const onInputHandler = (e, appointmentId, name) => {
+    setCanShowSaveBtn(true)
+    console.log(name, appointmentId, e.target.innerText)
+  }
+
+  return (
+    <table>
+      <caption>
+        Motif: <span>{baseAppointment.motif}</span>
+        <br />
+        Plan: <span>{baseAppointment.treatmentPlan}</span>
+        <br />
+        Reste: <span>{baseAppointment.totalPrice - totalPayments || '0'}</span>
+      </caption>
+      <thead>
+        <tr>
+          <th>titre</th>
+          <th>versement / prix-total</th>
+          <th>est fini</th>
+          <th>date de création</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th
+            contentEditable
+            suppressContentEditableWarning
+            onInput={(e) => onInputHandler(e, baseAppointment._id, ADD_APPOINTMENT_NAMES.TITLE)}>
+            {baseAppointment.title}
+          </th>
+          <th>
+            <span
+              contentEditable
+              suppressContentEditableWarning
+              onInput={(e) => onInputHandler(e, baseAppointment._id, ADD_APPOINTMENT_NAMES.PAYMENT)}
+              style={{ minWidth: '', padding: '2px 8px', outlineColor: '#587ee9' }}>
+              {baseAppointment.payment}
+            </span>
+            /
+            <span
+              contentEditable
+              suppressContentEditableWarning
+              onInput={(e) => onInputHandler(e, baseAppointment._id, ADD_APPOINTMENT_NAMES.TOTAL_PRICE)}
+              style={{ minWidth: '', padding: '2px 8px', outlineColor: '#587ee9' }}>
+              {baseAppointment.totalPrice}
+            </span>
+          </th>
+          <th>{appointmentsGroup.reduce((count, appointment) => (appointment.isDone ? count + 1 : count), 0)}</th>
+          <th>{format(parseISO(baseAppointment.createdAt), 'yyyy-MM-dd')}</th>
+        </tr>
+
+        {appointmentsGroup.map((appointment, index) => {
+          if (index === 0) return null
+          const { _id, createdAt, title, payment, isDone } = appointment
+
+          return (
+            <tr key={_id}>
+              <td
+                contentEditable
+                suppressContentEditableWarning
+                onInput={(e) => onInputHandler(e, _id, ADD_APPOINTMENT_NAMES.TITLE)}>
+                {title}
+              </td>
+              <td
+                contentEditable
+                suppressContentEditableWarning
+                onInput={(e) => onInputHandler(e, _id, ADD_APPOINTMENT_NAMES.PAYMENT)}>
+                {payment || '0'}
+              </td>
+              <td>{isDone ? 'Oui' : 'No'}</td>
+              <td>{format(parseISO(createdAt), 'yyyy-MM-dd')}</td>
+            </tr>
+          )
+        })}
+      </tbody>
+      {canShowSaveBtn && (
+        <tfoot>
+          <tr>
+            <td style={{ border: 'none', padding: '1rem 0' }}>
+              <Button type="submit" colorScheme="orange" mr={3}>
+                Sauvegarder rendez-vous
+              </Button>
+              <Button variant="ghost" onClick={() => setCanShowSaveBtn(false)}>
+                Annuler
+              </Button>
+            </td>
+          </tr>
+        </tfoot>
+      )}
+    </table>
+  )
+}
--- src/components/PatientFollowupsModal/PatientFollowupsModal.jsx
+++ src/components/PatientFollowupsModal/PatientFollowupsModal.jsx
@@ -1,21 +1,13 @@
 import { useEffect, useState } from 'react'
-import {
-  Modal,
-  ModalBody,
-  ModalCloseButton,
-  ModalContent,
-  ModalHeader,
-  ModalOverlay,
-  Box,
-  Button,
-} from '@chakra-ui/react'
-import { format, parseISO } from 'date-fns'
+import { Modal, ModalBody, ModalCloseButton, ModalContent, ModalHeader, ModalOverlay, Box } from '@chakra-ui/react'
 import { ChevronDown, ChevronUp } from 'react-feather'
 
 import { ChatState } from '../../context'
 import { getPatient } from '../../utils'
+
 import Loader from '../Loader/Loader'
 import PatientEditBody from './PatientEditBody'
+import AppointmentTable from './AppointmentTable'
 
 import './PatientFollowupsModal.scss'
 
@@ -73,85 +65,7 @@ export default function PatientFollowupsModal({ isOpen, onClose }) {
         <ModalBody className="patient-followups-modal-body">
           <Loader loading={isLoading}>
             {appointments.map((appointmentsGroup, index) => (
-              <table key={index}>
-                <caption>
-                  Motif: <span>{appointmentsGroup[0].motif}</span>
-                  <br />
-                  Plan: <span>{appointmentsGroup[0].treatmentPlan}</span>
-                </caption>
-                <thead>
-                  <tr>
-                    <th>titre</th>
-                    <th>versement</th>
-                    <th>est fini</th>
-                    <th>date de création</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <th
-                      contentEditable
-                      suppressContentEditableWarning
-                      onInput={(e) => console.log(e.target.innerText, '2')}>
-                      {appointmentsGroup[0].title}
-                    </th>
-                    <th
-                      contentEditable
-                      suppressContentEditableWarning
-                      onInput={(e) => console.log(e.target.innerText, '3')}>
-                      {appointmentsGroup[0].totalPrice}
-                    </th>
-                    <th>
-                      {appointmentsGroup.reduce((count, appointment) => (appointment.isDone ? count + 1 : count), 0)}
-                    </th>
-                    <th
-                      contentEditable
-                      suppressContentEditableWarning
-                      onInput={(e) => console.log(e.target.innerText, '1')}>
-                      {format(parseISO(appointmentsGroup[0].createdAt), 'yyyy-MM-dd')}
-                    </th>
-                  </tr>
-
-                  {appointmentsGroup.map((appointment) => {
-                    const { _id, createdAt, title, payment, isDone } = appointment
-                    return (
-                      <tr key={_id}>
-                        <td
-                          contentEditable
-                          suppressContentEditableWarning
-                          onInput={(e) => console.log(e.target.innerText, '5')}>
-                          {title}
-                        </td>
-                        <td
-                          contentEditable
-                          suppressContentEditableWarning
-                          onInput={(e) => console.log(e.target.innerText, '6')}>
-                          {payment}
-                        </td>
-                        <td>{isDone ? 'Oui' : 'No'}</td>
-                        <td
-                          contentEditable
-                          suppressContentEditableWarning
-                          onInput={(e) => console.log(e.target.innerText, '4')}>
-                          {format(parseISO(createdAt), 'yyyy-MM-dd')}
-                        </td>
-                      </tr>
-                    )
-                  })}
-                </tbody>
-                <tfoot>
-                  <tr>
-                    <td>
-                      <Button type="submit" colorScheme="orange" mr={3}>
-                        Sauvegarder rendez-vous
-                      </Button>
-                      <Button variant="ghost" onClick={() => {}}>
-                        Annuler
-                      </Button>
-                    </td>
-                  </tr>
-                </tfoot>
-              </table>
+              <AppointmentTable key={index} appointmentsGroup={appointmentsGroup} />
             ))}
           </Loader>
         </ModalBody>
