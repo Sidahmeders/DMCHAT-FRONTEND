d3b9a9c646d6629c8dabf6ad652d2a292f3888fd
= <=>
2023-06-24 14:14:22 +0100

    remove ADD_APPOINTMENT_NAMES

--- src/config/patient.js
+++ src/config/patient.js
@@ -4,15 +4,3 @@ export const CREATE_PATIENT_NAMES = {
   PHONE_NUMBER: 'phoneNumber',
   GENERAL_STATE: 'generalState',
 }
-
-export const ADD_APPOINTMENT_NAMES = {
-  FULL_NAME: 'fullName',
-  TITLE: 'title',
-  MOTIF: 'motif',
-  DIAGNOSTIC: 'diagnostic',
-  TREATMENT_PLAN: 'treatmentPlan',
-  TOTAL_PRICE: 'totalPrice',
-  PAYMENT: 'payment',
-  IS_NEW_TREATMENT: 'isNewTreatment',
-  BASE_APPOINTMENT_ID: 'baseAppointmentId',
-}
