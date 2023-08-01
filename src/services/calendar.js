5488fa75f06ed5ea43ed237a0608e721e24326a3
Ahmed Boutaraa <ahorob57@gmail.com>
2023-08-01 19:30:04 +0100

    refactor fetchMonthCalendarAvailability

--- src/services/calendar.js
+++ src/services/calendar.js
@@ -1,7 +1,9 @@
 import { formatDate } from '@utils'
 import _fetch from './_fetch'
 
-const fetchCalendarAvailabilities = async (date) => await _fetch.GET(`/api/calendar/${formatDate(date, 'yyyy/MM')}`)
+const fetchCalendarAvailabilities = async (date) => {
+  return await _fetch.GET(`/api/calendar/${formatDate(date, 'yyyy/MM')}/availability`)
+}
 
 const setCalendarAvailability = async (date, availability) => {
   return await _fetch.PUT(`/api/calendar/${formatDate(date, 'yyyy/MM/dd')}/availability`, { availability })
